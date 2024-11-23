566c0919a1cdce56824f11cd2db9970ea319c7b3
igortara <153300395+igortara@users.noreply.github.com>
2024-11-23 23:51:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function downloadLauncher() {
+    window.location.href = "https://drive.usercontent.google.com/u/0/uc?id=1pn7qsHs8LU6wa3D7AurJrGqdHvAZub4N&export=download"; // Укажите ссылку на лаунчер
+}
